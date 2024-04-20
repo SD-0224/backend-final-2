34b359239f93d00ddf81377ed9f349f529514d51
Muna-Zeer <munaelzeer@gmail.com>
2024-04-20 22:50:50 +0300

    logging-configuration

---
+++ src/config/pino.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import fs from "fs";
+//Define the logging  configuration  interface 
+interface PinoConfig{
+level: string,
+dest:string,
+}
+
+
+const logDir=path.join(__dirname,'logs');
+if(!fs.existsSync(logDir)){
+    fs.mkdirSync(logDir);
+}
+
+//Define the path of log file
+const logFile=path.join(logDir,'app.log');
+
+//Define the logging configuration object
+
+export const config: PinoConfig={
+level:'info',
+dest:logFile
+}
