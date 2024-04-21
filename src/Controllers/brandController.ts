678f1f72a680ce42765212c07f1705543c8b92ea
Muna-Zeer <munaelzeer@gmail.com>
2024-04-21 09:26:16 +0300

    cart-delete-get-debugging

--- src/Controllers/brandController.ts
+++ src/Controllers/brandController.ts
@@ -1,16 +1,18 @@
 import * as db from "../Models/index";
 import { sequelize } from "../config/dbConfig";
 import { Request, Response } from "express";
-
+import {logger} from "../config/pino";
 import brandServices from "../Services/brandServices";
 
 export const getAllBrands = async (req: Request, res: Response) => {
   try {
+    logger.info("Request to get all bands is received")
     const brands = await brandServices.getAllBrands();
+    logger.info("Request to get all brand is successful")
     res.status(200).json(brands);
   } catch (error) {
     // Handle errors and send appropriate error response
-    console.log("Error getting brands", error);
+    logger.error("Error getting brands");
     res.status(error.status).json({ error: error.message });
   }
 };
--- src/Controllers/cartController.ts
+++ src/Controllers/cartController.ts
@@ -4,54 +4,64 @@ import * as db from "../Models/index";
 import { sequelize } from "../config/dbConfig";
 import { Request, Response } from "express";
 import { Product } from "../Models/product";
+import { logger } from "../config/pino";
 //Get cart by userId and create a new cart objet
 export const cartController = {
   getCartByUserId: async (req: Request, res: Response) => {
     try {
       let userId = req.params.userId;
-      console.log("userId", userId);
+      logger.info(`Request received to get cart for ${userId} `);
 
       const cartItems = await CartItem.findAll({
         where: { userID: userId },
         include: [{ model: Product }],
       });
-      console.log("cartItems",cartItems);
-      
+      logger.info(`Cart Item retrieved successfully for ${userId} `);
+
       if (cartItems.length === 0) {
-        res.status(404).json({ error: "Cart is empty" });
+        logger.info(`Cart is empty for user ${userId}`);
+        return res.status(404).json({ error: "Cart is empty" });
       }
       res.json(cartItems);
-      // res.send({ message: `Getting ${userId} from cart` });
     } catch (error) {
-      console.log("Error fetching the catItems", error);
+      logger.error("Error fetching the catItems");
       res.status(500).json({ error: "Internal server error" });
     }
   },
   addItemsToCart: async (req: Request, res: Response) => {
     try {
       const userId = req.params.userID;
+      logger.info(`Checking if ${userId} exists in the cart `);
       const productId = req.params.productID;
       const { quantity } = req.body;
 
       // Check if the user exists
       const user = await db.User.findByPk(userId);
       if (!user) {
+        logger.error(`User ${userId} not found`);
         return res.status(404).json({ error: "User not found" });
       }
       //Checks if the product already exists
       const productItem = await db.Product.findByPk(productId);
+      logger.info("Product already exists" + productItem);
       if (!productItem) {
+        logger.error(`Product ${productId} not found`);
         return res.status(404).json({ error: "Product not found" });
       }
+      logger.info(`Product ${productId} found: ${JSON.stringify(productItem)}`);
 
       // Create or find the cart based on the userID
       let cart = await Cart.findOne({ where: { userID: userId } });
 
       if (!cart) {
-        // Create the cart
+        logger.error(
+          `Cart not found for user ${userId},please create new one `
+        );
         try {
           cart = await Cart.create({ userID: userId });
+          logger.info(`Cart created successfully`);
         } catch (error) {
+          logger.error("Error creating cart for the user");
           return res.status(500).json({ error: "Error creating cart" });
         }
       }
@@ -64,7 +74,11 @@ export const cartController = {
           productID: productId,
           productQuantity: quantity,
         });
+        logger.info("Cart created successfully");
       } catch (error) {
+        logger.error(
+          `Error creating cart item for user ${userId}: ${error.message}`
+        );
         return res.status(500).json({ error: "Error creating cart item" });
       }
 
@@ -78,37 +92,43 @@ export const cartController = {
           subtotal: productItem.price * quantity,
         },
       });
+      logger.info(`Adding ${productId} to cart for the user ${userId}`);
     } catch (error) {
-      console.error(error);
+      logger.error("Internal server Error", error);
       res.status(500).json({ error: "Internal server Error" });
     }
   },
   deleteItemsFromCart: async (req: Request, res: Response) => {
     try {
       const { userId, productId } = req.body;
+      logger.info(`deleting ${productId} from ${userId}`);
       await CartItem.destroy({ where: { userId, productId } });
-      res.json({ message: `Item deleted from the  cart for ${userId}` });
+      logger.info(`Deleted ${productId} from ${userId} successfully `);
+      return res.json({ message: `Item deleted from the  cart for ${userId}` });
     } catch (error) {
+      logger.error("Error deleting item from the cart", error);
       res.json({ error: "Internal server Error" });
     }
   },
   clearCart: async (req: Request, res: Response) => {
     try {
       let userId = req.body.userId;
-      console.log("userId",userId);
-      
+      logger.info(`Clearing ${userId} from the cart`);
       const deletedCount = await CartItem.destroy({
         where: { userID: userId },
       });
-      console.log("deletedCount",deletedCount);
+      logger.info(`Deleted ${deletedCount} from the cart`);
       if (deletedCount > 0) {
-        res.json({message:`Cart cleared successfully for user${userId}`})
-      }
-      else{
-
-        res.json({ message: ` No cartItem found  for ${userId}` });
+        logger.info(`Cart cleared successfully for user${userId}`);
+        return res.json({
+          message: `Cart cleared successfully for user${userId}`,
+        });
+      } else {
+        logger.error(`There is no ${userId} found in the cart`);
+        return res.json({ message: ` No cartItem found  for ${userId}` });
       }
     } catch (error) {
+      logger.error("Internal server Error", error);
       res.json({ error: "Internal server Error" });
     }
   },
