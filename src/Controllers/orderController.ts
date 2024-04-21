ff894c97299c8ad7353a54cbe70e5aaa42e14f96
Muna-Zeer <munaelzeer@gmail.com>
2024-04-21 16:03:09 +0300

    logging-create-order

--- src/Controllers/orderController.ts
+++ src/Controllers/orderController.ts
@@ -1,7 +1,7 @@
 import * as db from "../Models/index";
 import { sequelize } from "../config/dbConfig";
 import { Request, Response } from "express";
-
+import { logger } from "../config/pino";
 import orderServices from "../Services/orderServices";
 import productServices from "../Services/productServices";
 
@@ -9,19 +9,21 @@ export const createOrder = async (req: Request, res: Response) => {
   try {
     const transaction = await sequelize.transaction();
 
-    const { firstName, lastName, email, phoneNumber } = req.body
+    const { firstName, lastName, email, phoneNumber } = req.body;
 
-    const { street, state, city, pinCode } = req.body
-    const orderItems = req.body.orderItems
-    if(!orderItems){
-        return res.status(400).json({error: "Invalid Input"});
-      }
-      let grandTotal = 0
+    const { street, state, city, pinCode } = req.body;
+    const orderItems = req.body.orderItems;
+    if (!orderItems) {
+      logger.error("Invalid orderItems Value");
+      return res.status(400).json({ error: "Invalid Input" });
+    }
+    let grandTotal = 0;
+    logger.info("Order created successfully");
 
     res.status(200).json();
   } catch (error) {
     // Handle errors and send appropriate error response
-    console.log("Error creating order", error);
+    logger.error("Error creating order", error);
     res.status(error.status).json({ error: error.message });
   }
 };
