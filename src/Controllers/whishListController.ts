b4df14307af7580bbd87437fa5555278abc28ab8
Muna-Zeer <munaelzeer@gmail.com>
2024-04-24 16:05:34 +0300

    whishList-logging

--- src/Controllers/whishListController.ts
+++ src/Controllers/whishListController.ts
@@ -3,18 +3,18 @@ import { Product } from "../Models/product";
 import { User } from "../Models/user";
 import { Request, Response } from "express";
 import { whitelist } from "validator";
-
+import {logger}from "../config/pino";
 export const whishListController = {
   toggleWhishListProducts: async (req: Request&{userID:Number}, res: Response) => {
     try {
       const token = req.cookies.token;
       if (!token) {
-        // logger.error("Token not found in cookies");
+        logger.error("Token not found in cookies");
         return res.status(401).json({ error: "Unauthorized: Token not found" });
       }
       const { userID, productID } = req.body;
       if (!userID) {
-        // logger.error("Unauthorized: User can't create the order ");
+        logger.error("Unauthorized: User can't add whishList Items to the list  ");
         return res
           .status(401)
           .json({ error: "Unauthorized: User does not have permission" });
@@ -26,6 +26,7 @@ export const whishListController = {
 
       //Remove the Item from the list if it exists
       if (ItemExisted) {
+        logger.info("Removing existing Items  from the list");
         await ItemExisted.destroy();
         res.json({ message: "Item already exits" });
       } else {
@@ -34,17 +35,19 @@ export const whishListController = {
         res.status(201).json({message:"Item added successfully to whishList"})
       }
     } catch (error) {
-      console.log("error",error);
-      
+      logger.error("Internal server error" ,error);
       res.status(500).json({ error: "Internal server error" });
     }
   },
   getWhishListByUserId:async(req:Request&{userID:Number},res:Response)=>{
 try {
     const {userId}=req.params;
+    logger.info(`Get the list of favorites items for the user ${userId}`);
     const userWishList=await wishList.findAll({where:{userID:userId}});
+    logger.info("Whish list of items returned successfully");
     res.status(200).json({message:"Whish list of items returned successfully",userWishList},);
 } catch (error) {
+  logger.error("Internal service error",error)
     res.status(500).json({error:"Internal service error"});
 }
   }
