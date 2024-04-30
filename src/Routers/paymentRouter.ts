00c6eabe29afedcf72f41c10e3d764d1a674e285
Muna-Zeer <munaelzeer@gmail.com>
2024-04-30 19:36:39 +0300

    payment-routes

--- src/Routers/paymentRouter.ts
+++ src/Routers/paymentRouter.ts
@@ -1,4 +1,19 @@
 import express from "express";
 import { verifyToken } from "../Controllers/userController";
+import {paymentController} from "../Controllers/paymentController";
+export const paymentRouter=express.Router();
 
-const paymentRouter=express.Router();
+paymentRouter.post('/payWithUPI',verifyToken,paymentController.payWithUPI);
+  
+  paymentRouter.post('/payWithAmazon',verifyToken,paymentController.payWithAmazon);
+  
+  paymentRouter.post('/payWithApple',verifyToken,paymentController.payWithApple);
+  
+  paymentRouter.post('/payWithGoogle',verifyToken,paymentController.payWithGoogle);
+  
+  paymentRouter.post('/payWithCreditOrDebitCard',verifyToken,paymentController.payWithCreditOrDebitCard);
+  
+  paymentRouter.post('/payWithPhonePe',verifyToken,paymentController.payWithPhonePe);
+  
+  paymentRouter.post('/payWithPayTm',verifyToken,paymentController.payWithPayTm);
+  
