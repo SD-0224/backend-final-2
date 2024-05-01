cdae652d069877cd6fd6b002879cc85088d16875
Hasan Abu Thiab <93782814+hassanthiab@users.noreply.github.com>
2024-05-01 15:15:04 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -102,7 +102,7 @@ app.use("/orders", orderRouter);
 syncModels()
   .then(() => {
     console.log("Database synced successfully");
-    app.listen(PORT, () => {
+    app.listen(PORT, process.env.HOST || 'localhost', () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
